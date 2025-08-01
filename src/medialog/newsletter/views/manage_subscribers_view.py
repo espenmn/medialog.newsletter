b6f3ca2fe5706e269b6c778bda20b156d8bafc4c
Espen <espen@medialog.no>
2025-08-01 12:31:05 +0200

    "Add view: manage-subscribers-view"

---
+++ src/medialog/newsletter/views/manage_subscribers_view.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# from medialog.newsletter import _
+from Products.Five.browser import BrowserView
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class IManageSubscribersView(Interface):
+    """ Marker Interface for IManageSubscribersView"""
+
+
+class ManageSubscribersView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('manage_subscribers_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
